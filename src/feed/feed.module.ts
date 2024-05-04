58fa2e25c6d56db4dfb71fff3c8554fabd5176d5
AlexLeoChumak <chumak1106@gmail.com>
2024-05-04 21:33:05 +0300

    add imports: [TypeOrmModule.forFeature([FeedPostEntity])], providers: [Repository],

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { FeedPostEntity } from './models/feedPost.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { FeedService } from './services/feed.service';
 import { FeedController } from './controllers/feed.controller';
+import { Repository } from 'typeorm';
 
 @Module({
-  providers: [FeedService],
+  imports: [TypeOrmModule.forFeature([FeedPostEntity])],
+  providers: [FeedService, Repository],
   controllers: [FeedController],
 })
 export class FeedModule {}
