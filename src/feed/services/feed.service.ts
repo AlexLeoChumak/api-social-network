f1b94d6bbb49a32b560fa1c9ddec0b598f135448
AlexLeoChumak <chumak1106@gmail.com>
2024-05-04 21:33:51 +0300

    create method createPost

--- src/feed/services/feed.service.ts
+++ src/feed/services/feed.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+
+import { FeedPostEntity } from '../models/feedPost.entity';
+import { FeedPost } from '../models/feedPost.interface';
+import { Observable, from } from 'rxjs';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class FeedService {}
+export class FeedService {
+  constructor(
+    @InjectRepository(FeedPostEntity)
+    private readonly feedPostRepository: Repository<FeedPostEntity>,
+  ) {}
+
+  createPost(feedPost: FeedPost): Observable<FeedPost> {
+    return from(this.feedPostRepository.save(feedPost));
+  }
+}
