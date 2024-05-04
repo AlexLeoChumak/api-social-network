c7338cf11ec300039e1c538b3597734da60d0ac5
AlexLeoChumak <chumak1106@gmail.com>
2024-05-04 21:33:32 +0300

    create method @Post()   create

--- src/feed/controllers/feed.controller.ts
+++ src/feed/controllers/feed.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { FeedService } from '../services/feed.service';
+import { FeedPost } from '../models/feedPost.interface';
+import { Observable } from 'rxjs';
 
 @Controller('feed')
-export class FeedController {}
+export class FeedController {
+  constructor(private feedService: FeedService) {}
+
+  @Post()
+  create(@Body() post: FeedPost): Observable<FeedPost> {
+    return this.feedService.createPost(post);
+  }
+}
