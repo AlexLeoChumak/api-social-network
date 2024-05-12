c98b571dce0cd7e1e4095dc300d39d3a93a47675
AlexLeoChumak <chumak1106@gmail.com>
2024-05-12 21:24:06 +0300

    add @UseGuards(JwtGuard) to  @Post()   create

--- src/feed/controllers/feed.controller.ts
+++ src/feed/controllers/feed.controller.ts
@@ -7,19 +7,24 @@ import {
   Post,
   Put,
   Query,
+  Request,
+  UseGuards,
 } from '@nestjs/common';
-import { FeedService } from '../services/feed.service';
-import { FeedPost } from '../models/feedPost.interface';
 import { Observable } from 'rxjs';
 import { DeleteResult, UpdateResult } from 'typeorm';
+
+import { FeedService } from '../services/feed.service';
+import { FeedPost } from '../models/feedPost.interface';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('feed')
 export class FeedController {
   constructor(private feedService: FeedService) {}
 
+  @UseGuards(JwtGuard)
   @Post()
-  create(@Body() post: FeedPost): Observable<FeedPost> {
-    return this.feedService.createPost(post);
+  create(@Body() feedPost: FeedPost, @Request() req): Observable<FeedPost> {
+    return this.feedService.createPost(req.user, feedPost);
   }
 
   @Get()
