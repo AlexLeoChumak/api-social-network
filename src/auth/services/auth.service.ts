8422a61b76aad6e5f2d1594a8bb96deff160c950
AlexLeoChumak <chumak1106@gmail.com>
2024-05-12 21:23:43 +0300

    create AuthService

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,116 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Observable, catchError, from, map, switchMap, throwError } from 'rxjs';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+import { UserEntity } from '../models/user.entity';
+import { User } from '../models/user.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    private jwtService: JwtService,
+  ) {}
+
+  hashPassword(password: string): Observable<string> {
+    return from(bcrypt.hash(password, 12)).pipe(
+      catchError((err) => {
+        console.error(err);
+        return throwError(() => err);
+      }),
+    );
+  }
+
+  registerAccount(user: User): Observable<User> {
+    return this.hashPassword(user.password).pipe(
+      switchMap((hashedPassword: string) => {
+        return from(
+          this.userRepository.save({
+            ...user,
+            password: hashedPassword,
+          }),
+        );
+      }),
+      map((user: User) => {
+        delete user.password;
+        return user;
+      }),
+      catchError((err) => {
+        console.error(err);
+        return throwError(() => err);
+      }),
+    );
+  }
+
+  // метод registerAccount до оптимизации
+  //
+  // registerAccount(user: User): Observable<User> {
+  //   const { firstName, lastName, email, password } = user;
+
+  //   return this.hashPassword(password).pipe(
+  //     switchMap((hashedPassword: string) => {
+  //       return from(
+  //         this.userRepository.save({
+  //           firstName,
+  //           lastName,
+  //           email,
+  //           password: hashedPassword,
+  //         }),
+  //       ).pipe(
+  //         map((user: User) => {
+  //           delete user.password;
+  //           return user;
+  //         }),
+  //         catchError((err) => {
+  //           console.error(err);
+  //           return throwError(() => err);
+  //         }),
+  //       );
+  //     }),
+  //   );
+  // }
+
+  validateUser(email: string, password: string): Observable<User> {
+    return from(
+      this.userRepository.findOne({
+        where: { email },
+        select: ['id', 'firstName', 'lastName', 'email', 'password', 'role'],
+      }),
+    ).pipe(
+      switchMap((user: User) => {
+        return from(bcrypt.compare(password, user.password)).pipe(
+          map((isValidPassword: boolean) => {
+            if (isValidPassword) {
+              delete user.password;
+              return user;
+            }
+          }),
+        );
+      }),
+      catchError((err) => {
+        console.error(err);
+        return throwError(() => err);
+      }),
+    );
+  }
+
+  login(user: User): Observable<string> {
+    const { email, password } = user;
+
+    return this.validateUser(email, password).pipe(
+      switchMap((user: User) => {
+        if (user) {
+          return from(this.jwtService.signAsync({ user }));
+        }
+      }),
+      catchError((err) => {
+        console.error(err);
+        return throwError(() => err);
+      }),
+    );
+  }
+}
