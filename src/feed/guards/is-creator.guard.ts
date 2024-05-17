2cc622b8764989898a4813a6a8febc78f8e481bf
AlexLeoChumak <chumak1106@gmail.com>
2024-05-17 11:08:11 +0300

    create IsCreatorGuard implements CanActivate

---
+++ src/feed/guards/is-creator.guard.ts
@@ -0,0 +1,38 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable, map, switchMap } from 'rxjs';
+
+import { AuthService } from 'src/auth/services/auth.service';
+import { FeedService } from '../services/feed.service';
+import { User } from 'src/auth/models/user.interface';
+import { FeedPost } from '../models/feedPost.interface';
+
+@Injectable()
+export class IsCreatorGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private feedService: FeedService,
+  ) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { user, params }: { user: User; params: { id: number } } = request;
+
+    if (!user || !params) return false;
+    if (user.role === 'admin') return true;
+
+    const userId = user.id;
+    const feedId = params.id;
+
+    return this.authService
+      .findUserByid(userId)
+      .pipe(
+        switchMap((user: User) =>
+          this.feedService
+            .findPostById(feedId)
+            .pipe(map((feedPost: FeedPost) => user.id === feedPost.author.id)),
+        ),
+      );
+  }
+}
