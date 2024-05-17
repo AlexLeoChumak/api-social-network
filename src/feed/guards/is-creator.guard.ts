38e3d26205fd8e99ee91bf5c7afcc5b0c5e04424
AlexLeoChumak <chumak1106@gmail.com>
2024-05-17 16:13:22 +0300

    add error handler on canActivate(     context: ExecutionContext,   )

--- src/feed/guards/is-creator.guard.ts
+++ src/feed/guards/is-creator.guard.ts
@@ -1,5 +1,5 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable, map, switchMap } from 'rxjs';
+import { Observable, catchError, map, of, switchMap } from 'rxjs';
 
 import { AuthService } from 'src/auth/services/auth.service';
 import { FeedService } from '../services/feed.service';
@@ -19,20 +19,21 @@ export class IsCreatorGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const { user, params }: { user: User; params: { id: number } } = request;
 
-    if (!user || !params) return false;
-    if (user.role === 'admin') return true;
+    if (!user || !params) return of(false);
+    if (user.role === 'admin') return of(true);
 
     const userId = user.id;
     const feedId = params.id;
 
-    return this.authService
-      .findUserByid(userId)
-      .pipe(
-        switchMap((user: User) =>
-          this.feedService
-            .findPostById(feedId)
-            .pipe(map((feedPost: FeedPost) => user.id === feedPost.author.id)),
+    return this.authService.findUserByid(userId).pipe(
+      switchMap((user: User) =>
+        this.feedService.findPostById(feedId).pipe(
+          map((feedPost: FeedPost) => user.id === feedPost.author.id),
+
+          catchError(() => of(false)),
         ),
-      );
+      ),
+      catchError(() => of(false)),
+    );
   }
 }
