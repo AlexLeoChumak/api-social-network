54da58dc6d4cd7b13ef9365741f664f2e6db2d98
AlexLeoChumak <chumak1106@gmail.com>
2024-05-20 10:45:16 +0300

    change AuthService to UserService

--- src/feed/guards/is-creator.guard.ts
+++ src/feed/guards/is-creator.guard.ts
@@ -1,15 +1,15 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable, catchError, map, of, switchMap } from 'rxjs';
 
-import { AuthService } from 'src/auth/services/auth.service';
 import { FeedService } from '../services/feed.service';
 import { User } from 'src/auth/models/user.interface';
 import { FeedPost } from '../models/feedPost.interface';
+import { UserService } from 'src/auth/services/user.service';
 
 @Injectable()
 export class IsCreatorGuard implements CanActivate {
   constructor(
-    private authService: AuthService,
+    private userService: UserService,
     private feedService: FeedService,
   ) {}
 
@@ -25,11 +25,10 @@ export class IsCreatorGuard implements CanActivate {
     const userId = user.id;
     const feedId = params.id;
 
-    return this.authService.findUserByid(userId).pipe(
+    return this.userService.findUserByid(userId).pipe(
       switchMap((user: User) =>
         this.feedService.findPostById(feedId).pipe(
           map((feedPost: FeedPost) => user.id === feedPost.author.id),
-
           catchError(() => of(false)),
         ),
       ),
