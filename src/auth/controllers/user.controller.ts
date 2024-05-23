b2a47513b796cf8dcef740e4cd85536af839d0f9
AlexLeoChumak <chumak1106@gmail.com>
2024-05-23 13:15:47 +0300

    correct uploadImage(     @UploadedFile() file: Express.Multer.File,     @Request() req,   )

--- src/auth/controllers/user.controller.ts
+++ src/auth/controllers/user.controller.ts
@@ -10,7 +10,14 @@ import { FileInterceptor } from '@nestjs/platform-express';
 
 import { UserService } from '../services/user.service';
 import { JwtGuard } from '../guards/jwt.guard';
-import { saveImageToStorage } from '../helpers/image-storage';
+import {
+  isFileExtensionSafe,
+  removeFile,
+  saveImageToStorage,
+} from '../helpers/image-storage';
+import { Observable, of, switchMap } from 'rxjs';
+import { join } from 'path';
+import { UpdateResult } from 'typeorm';
 
 @Controller('user')
 export class UserController {
@@ -19,7 +26,27 @@ export class UserController {
   @UseGuards(JwtGuard)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file', saveImageToStorage))
-  uploadImage(@UploadedFile() file: Express.Multer.File, @Request() req): any {
-    return;
+  uploadImage(
+    @UploadedFile() file: Express.Multer.File,
+    @Request() req,
+  ): Observable<UpdateResult | { error: string }> {
+    const fileName = file?.filename;
+
+    if (!fileName) return of({ error: 'File must be a png, jpg/jpeg' });
+
+    const imagesFolderPath = join(process.cwd(), 'images');
+    const fullImagePath = join(`${imagesFolderPath}/${file.filename}`);
+
+    return isFileExtensionSafe(fullImagePath).pipe(
+      switchMap((isFileLegit: boolean) => {
+        if (isFileLegit) {
+          const userId = req.user.id;
+          return this.userService.updateUserImageById(userId, fileName);
+        }
+
+        removeFile(fullImagePath);
+        return of({ error: 'File content does not match extension' });
+      }),
+    );
   }
 }
