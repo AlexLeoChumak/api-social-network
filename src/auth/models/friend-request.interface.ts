2f231607fa0b97b12de221f89f673993f9cbcc94
AlexLeoChumak <chumak1106@gmail.com>
2024-05-30 09:33:00 +0300

    remove interface FriendRequestStatus as redundant

--- src/auth/models/friend-request.interface.ts
+++ src/auth/models/friend-request.interface.ts
@@ -2,11 +2,6 @@ import { User } from './user.interface';
 
 export type FriendRequestStatusType = 'pending' | 'accepted' | 'declined';
 
-//??????????
-export interface FriendRequestStatus {
-  status?: FriendRequestStatusType;
-}
-
 export interface FriendRequest {
   id?: number;
   creator?: User;
